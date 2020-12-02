cc9f94686802f9184c38b42a93f44b87d14b46f3
MangoMilk1911 <oliver.sarker1911@gmail.com>
2020-12-02 23:23:33 +1100

    Basic 🐸 Bot

---
+++ bot.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const BOT_PREFIX = "~"
+const FROG_PIC = "froggy"
+
+client.on('ready', () => {
+    console.log('Ribbit 🐸');
+});
+
+client.on('message', msg => {
+    if (msg.content === `${BOT_PREFIX}${FROG_PIC}`) {
+        sendFrog(msg)
+    }
+});
+
+function sendFrog(msg) {
+    const froggy = Math.floor(Math.random() * 54) + 1;
+    msg.channel.send(`http://www.allaboutfrogs.org/funstuff/random/00${froggy}.jpg`)
+    msg.react('🐸')
+}
+
+client.login(process.env.BOT_TOKEN);
